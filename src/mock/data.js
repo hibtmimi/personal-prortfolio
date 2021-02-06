21153a2b099283a7c39276bbcae78c8bff068e30
Hiba Tamimi <40838567+hibtmimi@users.noreply.github.com>
2021-02-07 01:41:44 +0200

    new project

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,14 +11,15 @@ export const headData = {
 export const heroData = {
   title: 'Hello, my name is',
   name: 'Hiba',
-  subtitle:"I'am a Front End Developer",
+  subtitle: "I'am a Front End Developer",
   cta: 'Know more',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'download.png',
-  paragraphOne: 'I am 24 Front End developer interested in code and design,  graduate from RBK(BootKamp) with a high level of commitment, organizing and communication skills   ',
+  paragraphOne:
+    'I am 24 Front End developer interested in code and design,  graduate from RBK(BootKamp) with a high level of commitment, organizing and communication skills   ',
   paragraphTwo: 'With experience in Html, CSS , JavaScript and React ',
   paragraphThree: 'Always and always hard work with creativity is the secret of success',
   resume: 'https://drive.google.com/drive/folders/1UI_lO6Ln_WKMrDKgJya9W4DD9TC657e0?usp=sharing', // if no resume, the button will not show up
@@ -30,7 +31,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'profile.PNG',
     title: 'LiLY',
-    info: 'Event web site , to help people find all the events in their places and book ticket to join it. ',
+    info:
+      'Event web site , to help people find all the events in their places and book ticket to join it. ',
     info2: ' Build with MERN technology (Mongodb , Express , React and Node.js',
     url: '',
     repo: 'https://github.com/lily-event-planner/lily', // if no repo, the button will not show up
@@ -39,12 +41,21 @@ export const projectsData = [
     id: nanoid(),
     img: 'project2.png',
     title: 'SHARKONA',
-     info: 'Event planing website , to help the couples in the planning and organizing process for their weeding day',
+    info:
+      'Event planing website , to help the couples in the planning and organizing process for their weeding day',
     info2: ' Build with MERN technology (Mongodb , Express , React and Node.js',
     url: '',
     repo: 'https://github.com/greenfield-sharkona/sharkona_v2', // if no repo, the button will not show up
   },
-
+  {
+    id: nanoid(),
+    img: 'Screenshot (12).png',
+    title: 'Amazon clone',
+    info: 'amazon clone website ',
+    info2: ' Build with React , express and firebase',
+    url: '',
+    repo: 'https://github.com/hibtmimi/amazon-clone.git', // if no repo, the button will not show up
+  },
 ];
 
 // CONTACT DATA
@@ -57,8 +68,6 @@ export const contactData = {
 // FOOTER DATA
 export const footerData = {
   networks: [
-
-
     {
       id: nanoid(),
       name: 'linkedin',
